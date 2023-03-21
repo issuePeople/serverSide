ad91dbb518d65fe20ce6f315e9ce1bf531191e88
Pau Duran <pau.duran.manzano@upc.edu>
2023-03-21 15:52:57 +0100

    Context per issue_list

--- issues/views.py
+++ issues/views.py
@@ -15,3 +15,11 @@ class CrearIssueView(FormView):
 class ListIssueView(TemplateView):
     model = models.Issue
     template_name = 'issue_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        issues = models.Issue.objects.all()
+        print("hola")
+        print(issues)
+        context.update({'issues': issues})
+        return context
