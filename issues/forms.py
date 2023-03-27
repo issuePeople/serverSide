da7b109f8b1122c9a2663d699ad0d36f6ab35741
Pau Duran <pau.duran.manzano@upc.edu>
2023-03-27 17:48:19 +0200

    Task #8: Afegim la resta de camps a la creació d'issues

--- issues/forms.py
+++ issues/forms.py
@@ -5,4 +5,4 @@ from issues import models
 class IssueForm(forms.ModelForm):
     class Meta:
         model = models.Issue
-        fields = ('subject', 'descripcio', 'assignacio')
+        fields = ('subject', 'descripcio', 'tipus', 'estat', 'gravetat', 'prioritat', 'assignacio', 'dataLimit')
