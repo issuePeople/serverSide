25d5df7948e4c08a2416845eec637b3087a9145f
Pau Duran <pau.duran.manzano@upc.edu>
2023-04-01 18:44:38 +0200

    task #87 enllaç a S3 AWS per guardar fitxers statics pel deploy

--- issuePeople/settings.py
+++ issuePeople/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'storages',
     'usuaris',
     'issues',
 ]
@@ -140,11 +141,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+AWS_ACCESS_KEY_ID = 'AKIAUENQNCDLSNV5XFN5'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'issuestorage'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
