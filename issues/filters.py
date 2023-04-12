61890b4872dbe83c47920b0fdb8f4b511e3e2e7d
Pau Duran <pau.duran.manzano@upc.edu>
2023-04-12 18:03:09 +0200

    task #126 filtrar fent servir __in a la BD, però transparent per forntend

--- issues/filters.py
+++ issues/filters.py
@@ -6,12 +6,25 @@ from .models import Issue
 
 class IssueFilter(django_filters.FilterSet):
     search = django_filters.CharFilter(method='search_filter', label=_('Search'))
+    tipus = django_filters.CharFilter(method='in_filter', label=_('Tipus'))
+    gravetat = django_filters.CharFilter(method='in_filter', label=_('Gravetat'))
+    prioritat = django_filters.CharFilter(method='in_filter', label=_('Prioritat'))
+    estat = django_filters.CharFilter(method='in_filter', label=_('Estat'))
+    assignacio = django_filters.CharFilter(method='in_filter', label=_('Assignació'))
+    tags = django_filters.CharFilter(method='in_filter', label=_('Creador'))
+    creador = django_filters.CharFilter(method='in_filter', label=_('Creador'))
 
     def search_filter(self, queryset, name, value):
         return queryset.filter(
             Q(subject__icontains=value) |
             Q(descripcio__icontains=value)
         )
+
+    def in_filter(self, queryset, name, value):
+        # Rebem les choices
+        choices = value.split(',')
+        # Filtrem per __in el queryset
+        return queryset.filter(**{f'{name}__in': choices})
 
     class Meta:
         model = Issue
