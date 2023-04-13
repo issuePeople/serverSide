173da7fa23631da39d3f96a77ee2b49648cde04a
Pau Duran <pau.duran.manzano@upc.edu>
2023-04-13 19:31:21 +0200

    Intentant solucionar el log in amb Google III

--- issuePeople/backends.py
+++ issuePeople/backends.py
@@ -1,5 +1,5 @@
 from storages.backends.s3boto3 import S3Boto3Storage
-from django.contrib.auth.backends import ModelBackend
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.contrib.auth.models import User
 from usuaris.models import Usuari
 
@@ -9,11 +9,38 @@ class MediaStorage(S3Boto3Storage):
     file_overwrite = False
 
 
-class CustomModelBackend(ModelBackend):
+class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     # Fem override the la creació d'usuaris quan fem log in amb Google
     # per crear instància d'Usuari (a més del User de Django)
-    def create_user(self, **kwargs):
-        user = User(**kwargs)
-        user.save(using=self._db)
-        Usuari.objects.create(user=user)
+    def save_user(self, request, sociallogin, form=None):
+        # Agafem el mail amb què s'ha fet login
+        email = sociallogin.account.extra_data['email']
+
+        # Mirem si trobem user
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            user = None
+
+        if user:
+            # Si existia, actualitzem valors
+            user.first_name = sociallogin.account.extra_data.get('first_name', '')
+            user.save()
+        else:
+            # Si no, creem usuari de django i nostre
+            user = User(
+                username=email,
+                email=email,
+                first_name=sociallogin.account.extra_data.get('first_name', '')
+            )
+            user.set_unusable_password()
+            user.save()
+
+            Usuari.objects.create(
+                user=user
+            )
+
+        # Enllacem la social account al user
+        sociallogin.account.user = user
+        sociallogin.account.save()
         return user
