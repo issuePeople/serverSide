09272fabf5709247b909b723682cc26151f6b36c
Pau Duran <pau.duran.manzano@upc.edu>
2023-04-24 15:30:41 +0200

    task #157 llistar, afegir i esborrar tags dels issues

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters, mixins, status
 from rest_framework.response import Response
-from issues.models import Issue
+from issues.models import Issue, Tag
 from usuaris.models import Usuari
 from . import serializers
 
@@ -86,6 +86,51 @@ class ObservadorsView(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.Des
 
         # Esborrem la relació entre issue i observador
         issue.observadors.remove(usuari)
+        issue.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class TagsIssueView(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        # Aconseguim l'issue donat per paràmetre
+        issue_id = self.kwargs['issue_id']
+        issue = get_object_or_404(Issue, id=issue_id)
+
+        # Filtrem per obtenir només els tags d'aquell issue
+        queryset = super().get_queryset()
+        queryset = queryset.filter(issues=issue)
+        return queryset
+
+    def create(self, request, *args, **kwargs):
+        # Per paràmetre ens ha de venir el nom i color del tag a afegir
+        nom = request.data.get('nom', None)
+        color = request.data.get('color', None)
+        if nom and color:
+            # Busquem el tag a afegir (si no existeix, el creem) i l'issue que hem de relacionar
+            tag, created = Tag.objects.get_or_create(nom=nom, defaults={'color': color})
+            issue_id = self.kwargs['issue_id']
+            issue = get_object_or_404(Issue, id=issue_id)
+
+            # Guardem la relació
+            issue.tags.add(tag)
+            issue.save()
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={
+                'error': "Has d'indicar l'identificador de l'observador a afegir"})
+
+    def destroy(self, request, *args, **kwargs):
+        # Tindrem /issues/issue_id/tags/pk: Agafem els paràmetres, l'issue i del tag
+        nom_tag = kwargs.get('pk')
+        tag = get_object_or_404(Tag, nom=nom_tag)
+        id_issue = kwargs.get('issue_id')
+        issue = get_object_or_404(Issue, id=id_issue)
+
+        # Esborrem la relació entre issue i observador
+        issue.tags.remove(tag)
         issue.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
